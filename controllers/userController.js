0cac4cb654c6190e17281e846c0a497384b57062
hajraahmad21 <hajra@karaz.app>
2024-09-25 06:05:29 +0500

    feat:Google Signup/signIn

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,8 +41,62 @@ const userController = {
         sameSite: "strict",
         maxAge: 24 * 60 * 60 * 1000, // 1 day
       });
-      return res.status(200).json({ message: "Login success", username: user.name, email:user.email, id:user._id });
+      return res.status(200).json({
+        message: "Login success",
+        username: user.name,
+        email: user.email,
+        id: user._id,
+      });
     })(req, res, next);
+  }),
+  googleAuth: passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+  googleAuthCallback: asyncHandler(async (req, res, next) => {
+    passport.authenticate(
+      "google",
+      { failureRedirect: "/login", session: false },
+      (err, user, info) => {
+        if (err) return next(err);
+        if (!user) {
+          return res.redirect("/http://localhost:5173/google-login-error");
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+          expiresIn: "3d",
+        });
+        // set token into cookies
+        res.cookie({
+          token: token,
+          httpOnly: true,
+          secure: false,
+          sameSite: "strict",
+          maxAge: 24 * 60 * 60 * 1000,
+        });
+        res.redirect("http://localhost:5173/dashboard");
+      }
+    )(req, res, next); //we are using iif (immediately invoked function expression) here to call the function
+  }),
+  // !Authentication Stattus
+  checkAuthenticated: asyncHandler(async (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded.id);
+      if (!user) {
+        return res.status(401).json({ message: "Unauthorized" });
+      } else {
+        return res.status(200).json({
+          status: "success",
+          message: "User found",
+          user,
+        });
+      }
+    } catch (err) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
   }),
 };
 module.exports = userController;
