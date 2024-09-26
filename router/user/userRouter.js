c81561684ea261a42775d4b8b58d87c9235977e3
hajraahmad21 <hajra@karaz.app>
2024-09-27 04:28:10 +0500

    feat:Add Logout Functionality

--- router/user/userRouter.js
+++ router/user/userRouter.js
@@ -6,4 +6,5 @@ userRouter.post("/login", UserController.login);
 userRouter.get("/auth/google", UserController.googleAuth);
 userRouter.get("/auth/callback", UserController.googleAuthCallback);
 userRouter.get("/checkAuthenticated", UserController.checkAuthenticated);
+userRouter.get("/logout", UserController.logout);
 module.exports = userRouter
